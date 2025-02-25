02b8ccf6fad8335f81ba5c167bd9e68cab23b93e
Elia <vagliettielia@gmail.com>
2025-02-25 17:08:57 +0000

    feat: writer linked to the PSO

--- include/output/writer_base.h
+++ include/output/writer_base.h
@@ -15,8 +15,8 @@ namespace pso
         std::size_t counter = 0;
         public:
 
-        virtual void write_square_domain(const Point<dim, T>& min, const Point<dim, T>& max, std::function<T(Point<dim, T>&)>& func) const = 0;
-        virtual void write_particle_position(const std::vector<Point<dim, T>>& particles, std::function<T(Point<dim, T>&)>& func) = 0; 
-        
+        virtual void write_square_domain(const Point<dim, T>& min, const Point<dim, T>& max, std::function<T(Point<dim, T>&)>& func) const { std::exit(1);};
+        virtual void write_particle_position(const std::vector<Point<dim, T>>& particles, std::function<T(Point<dim, T>&)>& func) {std::exit(1);}; 
+        ~WriterBase() = default;
     };
 }
--- test/function_test.cpp
+++ test/function_test.cpp
@@ -1,30 +1,30 @@
 #include "functions/standard_functions.h"
 #include "pso.h"
+#include "writer_txt.h"
 #include <functional>
 #include <cassert>
 
 int main()
 {
     double tol = 1e-10;
-
     double max = 10.0;
-    double min = 0.0;
-    const size_t dim = 3;
+    double min = -10.0;
+    const size_t dim = 2;
 
+    Point<dim, double> p1 = {1.0, 2.0};
 
-    Point<3, double> p1 = {1.0, 2.0, 3.0};
-
-    std::cout << sphere(p1) << std::endl;
-
-    std::function<double(Point<dim, double>&)> functional = sphere<3, double>;
-
-    std::function<double(Point<3, double>&)> f = sphere<3, double>;
+    // Test for the norm
+    std::function<double(Point<dim, double>&)> f = sphere<dim, double>;
     std::cout << f(p1) << std::endl;
     assert(sphere(p1) == f(p1));
 
+    // Real PSO example\
+    std::cout << sphere(p1) << std::endl;
+    std::function<double(Point<dim, double>&)> functional = sphere<dim, double>;
     Pso<dim, double> pso_test(dim*10, tol, 1000, max, min);
+    std::unique_ptr<WriterBase<dim, double>> p = std::make_unique<WriterTXT<dim, double>>();
+    pso_test.use_writer(std::move(p));
     pso_test.run_algorithm(functional);
-
     pso_test.print_global_best();
 
 }
