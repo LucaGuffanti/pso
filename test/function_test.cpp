b48e12e4156df98df3ad7cb3f9a927822134f460
Luca Guffanti <luca2.guffanti@mail.polimi.it>
2025-02-25 14:34:15 +0000

    feat: add debug mode

--- test/function_test.cpp
+++ test/function_test.cpp
@@ -1,8 +1,14 @@
 #include "functions/standard_functions.h"
+#include <functional>
+#include <cassert>
 
 int main()
 {
     Point<3, double> p1 = {1.0, 2.0, 3.0};
 
     std::cout << sphere(p1) << std::endl;
+    std::function<double(Point<3, double>&)> f = sphere<3, double>;
+
+    std::cout << f(p1) << std::endl;
+    assert(sphere(p1) == f(p1));
 }
